f5b1c41a4c2836f5fe6430ad16673c52b90f5de7
DavidAbili1 <104179991+DavidAbili1@users.noreply.github.com>
2024-06-28 17:59:02 -0400

    Add files via upload
    interstitial ads has been added

--- GroupProject/app/src/main/java/com/example/groupproject/MainActivity.kt
+++ GroupProject/app/src/main/java/com/example/groupproject/MainActivity.kt
@@ -1,3 +1,4 @@
+
 package com.example.groupproject
 
 import android.content.Intent
@@ -8,6 +9,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,12 +26,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var b1 : Button
     private lateinit var line : EditText
     private lateinit var the_message : TextView
+    private lateinit var ad : InterstitialAd
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
     }
 
     fun launchNumTest(view : View){
@@ -47,6 +52,26 @@ class MainActivity : AppCompatActivity() {
         } else {
             the_message.text = "NUMBER MUST BE BETWEEN 1 AND 25"
         }
+    }
 
+    inner class AdLoad : InterstitialAdLoadCallback() {
+
+        override fun onAdLoaded(p0: InterstitialAd) {
+            super.onAdLoaded(p0)
+            ad = p0
+            ad.show(this@MainActivity)
+        }
+
+        override fun onAdFailedToLoad(p0: LoadAdError) {
+            super.onAdFailedToLoad(p0)
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        var adUnitId : String = "ca-app-pub-3940256099942544/1033173712"
+        var adRequest : AdRequest = AdRequest.Builder().build()
+        var adLoad : AdLoad = AdLoad()
+        InterstitialAd.load( this, adUnitId, adRequest, adLoad )
     }
 }
