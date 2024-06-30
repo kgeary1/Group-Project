d659f17de5e7ab2cbc36bf7f8dd667967ef619b3
bernard0331 <47488073+bernard0331@users.noreply.github.com>
2024-06-29 23:23:39 -0400

    Added game functionality. The works as pseudo geoguesser but if the user doesn't want to play the game wherever they click it displays the coordinates of.

--- GroupProject/app/src/main/java/com/example/groupproject/MapGameActivity.kt
+++ GroupProject/app/src/main/java/com/example/groupproject/MapGameActivity.kt
@@ -1,12 +1,16 @@
 package com.example.groupproject
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.location.Address
 import android.location.Geocoder
 import android.location.Geocoder.GeocodeListener
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdate
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -16,29 +20,85 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlin.random.Random
 
 class MapGameActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var map : GoogleMap
+    private lateinit var timer : CountDownTimer
+    private lateinit var timerView : TextView
+    private lateinit var latLngView : TextView
+    private lateinit var playerClick : TextView
+    private lateinit var currLatLng : LatLng
+    private var mapGame : MapGame = MapGame()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_map)
-
-        var mapFragment : SupportMapFragment = supportFragmentManager.findFragmentById( R.id.map ) as SupportMapFragment
+        currLatLng = LatLng(0.0, 0.0)
+        timerView = findViewById(R.id.timer)
+        latLngView = findViewById(R.id.location)
+        playerClick = findViewById(R.id.player_click)
+        val mapFragment : SupportMapFragment = supportFragmentManager.findFragmentById( R.id.map ) as SupportMapFragment
 
         mapFragment.getMapAsync( this )
     }
 
     override fun onMapReady(p0: GoogleMap) {
         map = p0
-        var whiteHouse : LatLng = LatLng( 38.8977, -77.0366  )
-        var camera : CameraUpdate = CameraUpdateFactory.newLatLngZoom( whiteHouse, 18.0f )
-        map.moveCamera( camera)
+        val mh : MapClickHandler = MapClickHandler()
+        map.setOnMapClickListener(mh)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(37.0, -96.0), 4.0f))
+    }
+
+    inner class MapClickHandler : GoogleMap.OnMapClickListener {
+        override fun onMapClick(p0: LatLng) {
+            currLatLng = p0
+            Log.w("MapGameActivity", p0.toString())
+            playerClick.text = "Latitude: " + String.format("%.2f", p0.latitude) + "\nLongitude: " + String.format("%.2f", p0.longitude)
+        }
+
+    }
+
+    fun endMessage(bool : Boolean) {
+        if(bool) {
+            Toast.makeText(this, "You Win!", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "You Lose!", Toast.LENGTH_LONG).show()
+        }
     }
 
     fun goHome(view: View) {
         finish()
+    }
+
+    fun startGame(view: View) {
+        val lat : Double = Random.nextDouble(mapGame.bottom, mapGame.top)
+        val lon : Double = Random.nextDouble(mapGame.left, mapGame.right)
+        playerClick.text = ""
+        latLngView.text = "Latitude: " + String.format("%.2f", lat) + "\nLongitude: " + String.format("%.2f", lon)
+        timerView.text = "20"
+        timer = object : CountDownTimer(20000, 1) {
+            override fun onTick(millisUntilFinished: Long) {
+                timerView.text = (millisUntilFinished/1000.0).toString()
+                if (mapGame.checkDistance(lat, lon, currLatLng.latitude, currLatLng.longitude)) {
+                    onFinish()
+                    this.cancel()
+                }
+            }
+
+            override fun onFinish() {
+                if (mapGame.checkDistance(lat, lon, currLatLng.latitude, currLatLng.longitude)) {
+                    endMessage(true)
+                } else {
+                    endMessage(false)
+                    timerView.text = (0.000).toString()
+                }
+            }
+        }
+        timer.start()
+        timerView.text = ""
     }
 
 
